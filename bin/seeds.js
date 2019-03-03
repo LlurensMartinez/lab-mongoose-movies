c7c2c465adf0fc50588134a54d245ea11f4ffef6
Llorenç <crack.one.7@gmail.com>
2019-03-03 20:41:42 +0100

    config and seeds.js

---
+++ bin/seeds.js
@@ -0,0 +1,16 @@
+module.exports = [{
+  name: 'Heisenberg',
+  occupation: 'Chemical',
+  catchPhrase: 'Say my name!'
+},
+{
+  name: 'Jesse Pinkman',
+  occupation: 'Chemical and Yonki',
+  catchPhrase: 'Where is my money, bitch!'
+},
+{
+  name: 'Saul',
+  occupation: 'lawyer ',
+  catchPhrase: 'Better call Saúl'
+}
+];
